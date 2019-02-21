a4bc29a3ee5b2a0f9569ae82fd833383c73d0360
pleyn <45330949+pleyn@users.noreply.github.com>
2019-02-21 17:12:00 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>CI Demo</title>
  </head>
  <body>
- <?php echo '<p>Petit Hello World basique</p>'; echo '<p>Test</p>'; ?> 
+ <?php echo '<p>Petit Hello World basique</p>'; echo '<p>Coucou</p>'; ?> 
  </body>
 </html>
